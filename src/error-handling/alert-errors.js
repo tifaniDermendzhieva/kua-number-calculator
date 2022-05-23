60f81373f49ad3cb2ef6cdd4eecfaa5cf00e4c7d
tifani <95909773+tifaniDermendzhieva@users.noreply.github.com>
2022-05-23 16:58:30 +0300

    Delete alert-errors.js
    changed to alert-all-fields-required.js

--- src/error-handling/alert-errors.js
+++
@@ -1,7 +0,0 @@
-
-export function allInputFieldsRequired(...input) {
-    if (input.includes(undefined) || input.includes(null)) {
-        alert('All fields are required!');
-        throw new Error('invalid input');
-    }
-}
